dc9febec360a7415af8aba38b9f0c3b0b043c53a
ali-aga-ai <alimahdiiaga@gmail.com>
2024-10-18 00:52:34 +0530

    image transformation for phone

--- src/components/doc_head.js
+++ src/components/doc_head.js
@@ -4,15 +4,13 @@ const HealthPartnerSection = ({ handleButtonClick }) => {
   const image = require('./images/logo.jpeg')
 
   return (
-    <div className='flex flex-col md:flex-row '>
-      {' '}
-      {/* Use flex-col for mobile and flex-row for larger screens */}
+    <div className='flex flex-col md:flex-row'>
       {/* Left side with image */}
       <div className='w-full md:w-1/2'>
         <img
           src={image}
           alt='Medical professional in a hospital setting'
-          className='object-cover h-screen w-full'
+          className='w-full h-[300px] md:h-screen object-cover' // Reduced height on mobile
         />
       </div>
       {/* Right side with content */}
@@ -21,16 +19,12 @@ const HealthPartnerSection = ({ handleButtonClick }) => {
           {/* Content for the section */}
         </div>
         <div className='mt-5 md:mt-10'>
-          {' '}
-          {/* Adjust margin for mobile */}
           <h2 className='text-3xl md:text-5xl font-bold mb-3 md:mb-5'>
             YOUR PARTNER IN HEALTH
-          </h2>{' '}
-          {/* Adjust text size for mobile */}
+          </h2>
           <p className='text-lg md:text-xl mb-5 md:mb-10'>
             Personalized care that puts you first
-          </p>{' '}
-          {/* Adjust text size for mobile */}
+          </p>
           <button
             className='bg-transparent border border-black text-black py-2 px-6 md:py-2 md:px-10 hover:bg-white hover:text-black transition duration-300'
             onClick={() => handleButtonClick('cal')}
